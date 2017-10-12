18a11b4b4e5ace7683ca1b7217e63a309085aee5
Leni-Deskop\krycz <krzysztof@kryczek.pl>
2017-10-12 02:41:33 +0200

    edit js

--- js/get-post.js
+++ js/get-post.js
@@ -50,8 +50,7 @@ jQuery( function( $ ) {
 
 					// as long as we still have pages to load
 					if(fetchpage <= totalpages) {
-						setTimeout(function () {$('.load-more a').text('Load More News');
-          }, 2500);
+						setTimeout(function () {$('.load-more a').text('Load More News');}, 2500);
 					} else {
 						$('.load-more-button').hide();
 						$('.js-load-more').text('No more news to load.');
